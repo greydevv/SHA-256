6d0fc9a3a1e98304ffa7711fa88d9d63e77fdfcc
Greyson Murray <greyson.murray@gmail.com>
2021-09-19 16:40:49 -0400

    more comment cleanup

--- src/sha256.c
+++ src/sha256.c
@@ -42,24 +42,24 @@ void update_sha256(sha256_ctx* ctx, const char* msg, uint64_t msglen, char* buf)
     ctx->data[ctx->datalen++] = 0x80;
     if (ctx->datalen > 56)
     {
-        // current block will not be able to fit 64 bits on the end, so there needs to be another 512-bit block.
+        // current block will not be able to fit 64 bits on the end, so there needs to be another 512-bit block
         while (ctx->datalen < 64)
         {
             ctx->data[ctx->datalen++] = 0x00;
         }
         transform_sha256(ctx);
     }
-    // pad the message up to 448 bits.
+    // pad the message up to 448 bits
     while (ctx->datalen < 56)
     {
         ctx->data[ctx->datalen++] = 0x00;
     }
-    // here, 'ctx->data' is 448 bits long (56 bytes). This leaves 64 bits (8 bytes) for the message length.
+    // here, 'ctx->data' is 448 bits long (56 bytes). This leaves 64 bits (8 bytes) for the message length
     // total length should now be 512 bits.
     uint64_t msgbitlen = msglen*8;
     for (int i = 0; i < 8; i++)
     {
-        // replace the last 64 bits (8 bytes) with the length of the original message in bits.
+        // replace the last 64 bits (8 bytes) with the length of the original message in bits
         ctx->data[63-i] = msgbitlen >> (i*8);
         ctx->datalen++;
     }
